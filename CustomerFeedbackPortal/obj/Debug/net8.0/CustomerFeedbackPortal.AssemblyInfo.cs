c353726977249d64eb7a060aa21133dc38e300e3
Raghava Ranjith <jalluriraghava@gmail.com>
2025-06-13 11:39:09 +0530

    project

--- CustomerFeedbackPortal/obj/Debug/net8.0/CustomerFeedbackPortal.AssemblyInfo.cs
+++ CustomerFeedbackPortal/obj/Debug/net8.0/CustomerFeedbackPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomerFeedbackPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4801f887005bfaed6f88ceae0d8ce35f5a5ec99")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomerFeedbackPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomerFeedbackPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
